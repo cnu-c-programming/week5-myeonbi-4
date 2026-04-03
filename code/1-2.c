db143a2e7f3d26772e2d355303e0fdddf59725d1
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-04-03 12:24:39 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sum_range(int *start, int *end) {
+    int sum = 0;
+    for (int *p = start; p < end; p++) {
+        sum += *p;
+    }
+    return sum;
+}
+
+int main() {
+    int data[] = {10, 20, 30, 40, 50, 60, 70};
+
+    int result = sum_range(&data[1], &data[4]);
+
+    printf("%d\n", result);
+    return 0;
+}
